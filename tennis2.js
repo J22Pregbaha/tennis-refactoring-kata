6edff9e85b22b3fb4684519df73f84d8f5cba8ef
J22Pregbaha <jpregbaha@gmail.com>
2024-04-08 18:07:06 +0100

    Refactored tie score situation

--- tennis2.js
+++ tennis2.js
@@ -2,42 +2,35 @@
 
 const SCORE_NAMES = ['Love', 'Fifteen', 'Thirty', 'Forty'];
 
-function getScore(PlayerOneScore, playerTwoScore) {
+function getScore(playerOneScore, playerTwoScore) {
     let score = "";
 
-    if (PlayerOneScore === playerTwoScore && PlayerOneScore < 3) {
-        if (PlayerOneScore === 0) {
-            score = "Love";
+    // Tie game situation
+    if (playerOneScore === playerTwoScore) {
+        if (playerOneScore < 3) {
+            score = `${SCORE_NAMES[playerOneScore]}-All`;
+        } else {
+            score = "Deuce";
         }
-        if (PlayerOneScore === 1) {
-            score = "Fifteen";
-        }
-        if (PlayerOneScore === 2) {
-            score = "Thirty";
-        }
-        score += "-All";
-    }
-    if (PlayerOneScore === playerTwoScore && PlayerOneScore > 2) {
-        score = "Deuce";
     }
 
     var P1res;
     var P2res;
-    if (PlayerOneScore > 0 && playerTwoScore === 0) {
-        if (PlayerOneScore === 1) {
+    if (playerOneScore > 0 && playerTwoScore === 0) {
+        if (playerOneScore === 1) {
             P1res = "Fifteen";
         }
-        if (PlayerOneScore === 2) {
+        if (playerOneScore === 2) {
             P1res = "Thirty";
         }
-        if (PlayerOneScore === 3) {
+        if (playerOneScore === 3) {
             P1res = "Forty";
         }
 
         P2res = "Love";
         score = P1res + "-" + P2res;
     }
-    if (playerTwoScore > 0 && PlayerOneScore === 0) {
+    if (playerTwoScore > 0 && playerOneScore === 0) {
         if (playerTwoScore === 1) {
             P2res = "Fifteen";
         }
@@ -52,11 +45,11 @@ function getScore(PlayerOneScore, playerTwoScore) {
         score = P1res + "-" + P2res;
     }
 
-    if (PlayerOneScore > playerTwoScore && PlayerOneScore < 4) {
-        if (PlayerOneScore === 2) {
+    if (playerOneScore > playerTwoScore && playerOneScore < 4) {
+        if (playerOneScore === 2) {
             P1res = "Thirty";
         }
-        if (PlayerOneScore === 3) {
+        if (playerOneScore === 3) {
             P1res = "Forty";
         }
         if (playerTwoScore === 1) {
@@ -67,34 +60,34 @@ function getScore(PlayerOneScore, playerTwoScore) {
         }
         score = P1res + "-" + P2res;
     }
-    if (playerTwoScore > PlayerOneScore && playerTwoScore < 4) {
+    if (playerTwoScore > playerOneScore && playerTwoScore < 4) {
         if (playerTwoScore === 2) {
             P2res = "Thirty";
         }
         if (playerTwoScore === 3) {
             P2res = "Forty";
         }
-        if (PlayerOneScore === 1) {
+        if (playerOneScore === 1) {
             P1res = "Fifteen";
         }
-        if (PlayerOneScore === 2) {
+        if (playerOneScore === 2) {
             P1res = "Thirty";
         }
         score = P1res + "-" + P2res;
     }
 
-    if (PlayerOneScore > playerTwoScore && playerTwoScore >= 3) {
+    if (playerOneScore > playerTwoScore && playerTwoScore >= 3) {
         score = "Advantage player1";
     }
 
-    if (playerTwoScore > PlayerOneScore && PlayerOneScore >= 3) {
+    if (playerTwoScore > playerOneScore && playerOneScore >= 3) {
         score = "Advantage player2";
     }
 
-    if (PlayerOneScore >= 4 && playerTwoScore >= 0 && (PlayerOneScore - playerTwoScore) >= 2) {
+    if (playerOneScore >= 4 && playerTwoScore >= 0 && (playerOneScore - playerTwoScore) >= 2) {
         score = "Win for player1";
     }
-    if (playerTwoScore >= 4 && PlayerOneScore >= 0 && (playerTwoScore - PlayerOneScore) >= 2) {
+    if (playerTwoScore >= 4 && playerOneScore >= 0 && (playerTwoScore - playerOneScore) >= 2) {
         score = "Win for player2";
     }
     return score;
