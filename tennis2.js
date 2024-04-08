db647d19443a68074a9b5a8164a52c7d32ba20d0
J22Pregbaha <jpregbaha@gmail.com>
2024-04-08 18:05:06 +0100

    Changed variable names

--- tennis2.js
+++ tennis2.js
@@ -2,49 +2,49 @@
 
 const SCORE_NAMES = ['Love', 'Fifteen', 'Thirty', 'Forty'];
 
-function getScore(P1point, P2point) {
-    var score = "";
+function getScore(PlayerOneScore, playerTwoScore) {
+    let score = "";
 
-    if (P1point === P2point && P1point < 3) {
-        if (P1point === 0) {
+    if (PlayerOneScore === playerTwoScore && PlayerOneScore < 3) {
+        if (PlayerOneScore === 0) {
             score = "Love";
         }
-        if (P1point === 1) {
+        if (PlayerOneScore === 1) {
             score = "Fifteen";
         }
-        if (P1point === 2) {
+        if (PlayerOneScore === 2) {
             score = "Thirty";
         }
         score += "-All";
     }
-    if (P1point === P2point && P1point > 2) {
+    if (PlayerOneScore === playerTwoScore && PlayerOneScore > 2) {
         score = "Deuce";
     }
 
     var P1res;
     var P2res;
-    if (P1point > 0 && P2point === 0) {
-        if (P1point === 1) {
+    if (PlayerOneScore > 0 && playerTwoScore === 0) {
+        if (PlayerOneScore === 1) {
             P1res = "Fifteen";
         }
-        if (P1point === 2) {
+        if (PlayerOneScore === 2) {
             P1res = "Thirty";
         }
-        if (P1point === 3) {
+        if (PlayerOneScore === 3) {
             P1res = "Forty";
         }
 
         P2res = "Love";
         score = P1res + "-" + P2res;
     }
-    if (P2point > 0 && P1point === 0) {
-        if (P2point === 1) {
+    if (playerTwoScore > 0 && PlayerOneScore === 0) {
+        if (playerTwoScore === 1) {
             P2res = "Fifteen";
         }
-        if (P2point === 2) {
+        if (playerTwoScore === 2) {
             P2res = "Thirty";
         }
-        if (P2point === 3) {
+        if (playerTwoScore === 3) {
             P2res = "Forty";
         }
 
@@ -52,49 +52,49 @@ function getScore(P1point, P2point) {
         score = P1res + "-" + P2res;
     }
 
-    if (P1point > P2point && P1point < 4) {
-        if (P1point === 2) {
+    if (PlayerOneScore > playerTwoScore && PlayerOneScore < 4) {
+        if (PlayerOneScore === 2) {
             P1res = "Thirty";
         }
-        if (P1point === 3) {
+        if (PlayerOneScore === 3) {
             P1res = "Forty";
         }
-        if (P2point === 1) {
+        if (playerTwoScore === 1) {
             P2res = "Fifteen";
         }
-        if (P2point === 2) {
+        if (playerTwoScore === 2) {
             P2res = "Thirty";
         }
         score = P1res + "-" + P2res;
     }
-    if (P2point > P1point && P2point < 4) {
-        if (P2point === 2) {
+    if (playerTwoScore > PlayerOneScore && playerTwoScore < 4) {
+        if (playerTwoScore === 2) {
             P2res = "Thirty";
         }
-        if (P2point === 3) {
+        if (playerTwoScore === 3) {
             P2res = "Forty";
         }
-        if (P1point === 1) {
+        if (PlayerOneScore === 1) {
             P1res = "Fifteen";
         }
-        if (P1point === 2) {
+        if (PlayerOneScore === 2) {
             P1res = "Thirty";
         }
         score = P1res + "-" + P2res;
     }
 
-    if (P1point > P2point && P2point >= 3) {
+    if (PlayerOneScore > playerTwoScore && playerTwoScore >= 3) {
         score = "Advantage player1";
     }
 
-    if (P2point > P1point && P1point >= 3) {
+    if (playerTwoScore > PlayerOneScore && PlayerOneScore >= 3) {
         score = "Advantage player2";
     }
 
-    if (P1point >= 4 && P2point >= 0 && (P1point - P2point) >= 2) {
+    if (PlayerOneScore >= 4 && playerTwoScore >= 0 && (PlayerOneScore - playerTwoScore) >= 2) {
         score = "Win for player1";
     }
-    if (P2point >= 4 && P1point >= 0 && (P2point - P1point) >= 2) {
+    if (playerTwoScore >= 4 && PlayerOneScore >= 0 && (playerTwoScore - PlayerOneScore) >= 2) {
         score = "Win for player2";
     }
     return score;
