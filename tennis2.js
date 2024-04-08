ad61d81206b332490fc4dfcdd95f85262f86633e
J22Pregbaha <jpregbaha@gmail.com>
2024-04-08 18:13:24 +0100

    Refactored remaining situations

--- tennis2.js
+++ tennis2.js
@@ -3,94 +3,27 @@
 const SCORE_NAMES = ['Love', 'Fifteen', 'Thirty', 'Forty'];
 
 function getScore(playerOneScore, playerTwoScore) {
-    let score = "";
-
     // Tie game situation
     if (playerOneScore === playerTwoScore) {
         if (playerOneScore < 3) {
-            score = `${SCORE_NAMES[playerOneScore]}-All`;
+            return `${SCORE_NAMES[playerOneScore]}-All`;
         } else {
-            score = "Deuce";
+            return "Deuce";
         }
     }
 
-    var P1res;
-    var P2res;
-    if (playerOneScore > 0 && playerTwoScore === 0) {
-        if (playerOneScore === 1) {
-            P1res = "Fifteen";
+    // Advantage/Win situation
+    if (playerOneScore >= 4 || playerTwoScore >= 4) {
+        const scoreDifference = playerOneScore - playerTwoScore;
+        if (Math.abs(scoreDifference) === 1) {
+            return scoreDifference === 1 ? "Advantage player1" : "Advantage player2";
+        } else {
+            return scoreDifference >= 2 ? "Win for player1" : "Win for player2";
         }
-        if (playerOneScore === 2) {
-            P1res = "Thirty";
-        }
-        if (playerOneScore === 3) {
-            P1res = "Forty";
-        }
-
-        P2res = "Love";
-        score = P1res + "-" + P2res;
     }
-    if (playerTwoScore > 0 && playerOneScore === 0) {
-        if (playerTwoScore === 1) {
-            P2res = "Fifteen";
-        }
-        if (playerTwoScore === 2) {
-            P2res = "Thirty";
-        }
-        if (playerTwoScore === 3) {
-            P2res = "Forty";
-        }
-
-        P1res = "Love";
-        score = P1res + "-" + P2res;
-    }
-
-    if (playerOneScore > playerTwoScore && playerOneScore < 4) {
-        if (playerOneScore === 2) {
-            P1res = "Thirty";
-        }
-        if (playerOneScore === 3) {
-            P1res = "Forty";
-        }
-        if (playerTwoScore === 1) {
-            P2res = "Fifteen";
-        }
-        if (playerTwoScore === 2) {
-            P2res = "Thirty";
-        }
-        score = P1res + "-" + P2res;
-    }
-    if (playerTwoScore > playerOneScore && playerTwoScore < 4) {
-        if (playerTwoScore === 2) {
-            P2res = "Thirty";
-        }
-        if (playerTwoScore === 3) {
-            P2res = "Forty";
-        }
-        if (playerOneScore === 1) {
-            P1res = "Fifteen";
-        }
-        if (playerOneScore === 2) {
-            P1res = "Thirty";
-        }
-        score = P1res + "-" + P2res;
-    }
-
-    if (playerOneScore > playerTwoScore && playerTwoScore >= 3) {
-        score = "Advantage player1";
-    }
-
-    if (playerTwoScore > playerOneScore && playerOneScore >= 3) {
-        score = "Advantage player2";
-    }
-
-    if (playerOneScore >= 4 && playerTwoScore >= 0 && (playerOneScore - playerTwoScore) >= 2) {
-        score = "Win for player1";
-    }
-    if (playerTwoScore >= 4 && playerOneScore >= 0 && (playerTwoScore - playerOneScore) >= 2) {
-        score = "Win for player2";
-    }
-    return score;
+    
+    // Regular scoring
+    return `${SCORE_NAMES[playerOneScore]}-${SCORE_NAMES[playerTwoScore]}`;
 }
 
 module.exports = getScore;
